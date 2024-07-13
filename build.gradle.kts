d0248e9900ef8ccf17b801c0ad5a1f09d1b4a091
David Vega Lichacz <7826728+realdavidvega@users.noreply.github.com>
2024-07-13 21:16:34 +0200

    feat: base library structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,18 @@
-@file:Suppress("UnstableApiUsage")
-
 plugins {
-  java
-  alias(libs.plugins.kotlin.jvm)
-  alias(libs.plugins.spotless)
-  alias(libs.plugins.gradle.ktlint)
+  base
+  alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.spotless) apply false
+  alias(libs.plugins.gradle.ktlint) apply false
 }
 
-repositories {
-  mavenLocal()
-  mavenCentral()
+allprojects {
+  group = property("project.group").toString()
+  version = property("project.version").toString()
 }
-
-dependencies {
-  implementation(libs.bundles.kotlin)
-  testImplementation(libs.bundles.kotest)
-}
-
-kotlin { jvmToolchain(21) }
-
-java {
-  sourceCompatibility = JavaVersion.VERSION_21
-  targetCompatibility = JavaVersion.VERSION_21
-}
-
-spotless {
-  kotlin { ktfmt(libs.versions.ktfmt.get()).googleStyle() }
-  kotlinGradle { ktfmt(libs.versions.ktfmt.get()).googleStyle() }
-}
-
-ktlint { version.set(libs.versions.ktlint.get()) }
 
 tasks {
   wrapper {
     gradleVersion = libs.versions.gradle.wrapper.get()
     distributionType = Wrapper.DistributionType.BIN
   }
-  compileKotlin { compilerOptions { freeCompilerArgs.add("-Xcontext-receivers") } }
-  compileTestKotlin { compilerOptions { freeCompilerArgs.add("-Xcontext-receivers") } }
-  test { useJUnitPlatform() }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+pluginManagement {
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+// root project
 rootProject.name = "kotlin-init"
+
+// sub-projects
+include("kotlin-init-core")
+project(":kotlin-init-core").projectDir = file("core")
